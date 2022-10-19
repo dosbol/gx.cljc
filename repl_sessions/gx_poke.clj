3686fd05b2acd08b6122c039d42d9d353b85147a
Artem Medeu <artem.medeusheyev@gmail.com>
2022-10-19 09:46:45 +0600

    feat: add causes to error messages

---
+++ repl_sessions/gx_poke.clj
@@ -0,0 +1,22 @@
+(ns gx-poke
+  (:require [k16.gx.beta.system :as gx-sys]
+            [promesa.core :as p]))
+
+(def compo {:gx/start {:gx/processor (fn [_] (println "starting foo"))}})
+(def compo2 {:gx/start {:gx/processor (fn [_] (throw (Exception. "x")))}})
+(def compo3 {:gx/start {:gx/processor (fn [_] (println "starting 3"))}
+             :gx/stop {:gx/processor (fn [_] (println "stopping 3"))}})
+
+(gx-sys/register!
+ ::x
+ {:graph {:foo {:gx/component 'gx.poke/compo
+                :gx/props {:bar '(gx/ref :bar)}}
+          :bar {:gx/component 'gx-poke/compo2
+                :gx/props {:bar '(gx/ref :baq)}}
+          :baq {:gx/component `compo3}}})
+
+(comment
+  (gx-sys/failures-humanized ::x)
+  (gx-sys/signal! ::x :gx/start)
+  (gx-sys/states ::x)
+  )
